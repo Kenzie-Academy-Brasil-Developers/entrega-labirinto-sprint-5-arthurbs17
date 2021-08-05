9d71c1c2fdfc84c34da824b75a006b3934088b7b
Arthur Sarmento <arthurbs17@gmail.com>
2021-08-05 20:01:16 -0300

    Criação do labirinto melhorada

--- assets/script.js
+++ assets/script.js
@@ -2,41 +2,45 @@ const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
     "W   W     W     W W W",
     "W W W WWW WWWWW W W W",
-    "W W W   W     W W   W",
+    "W W W         W W   W",
     "W WWWWWWW W WWW W W W",
     "W         W     W W W",
     "W WWW WWWWW WWWWW W W",
     "W W   W   W W     W W",
     "W WWWWW W W W WWW W F",
-    "S     W W W W W W WWW",
+    "S      W W W W W W WWW",
     "WWWWW W W W W W W W W",
-    "W     W W W   W W W W",
+    "W     W W W   W W W W",
     "W WWWWWWW WWWWW W W W",
     "W       W       W   W",
     "WWWWWWWWWWWWWWWWWWWWW",
 ];
 
-function maze(){
+function buildMaze(){
     let maze = document.getElementById('maze');
+    let playerPosition = [];
 
     for (let i = 0; i < map.length; i++){
         let blockMaze = document.createElement('div');
         maze.appendChild(blockMaze);
+        blockMaze.classList.add('blockMaze')
         for(let j = 0; j < map[i].length; j++){
             let elementMaze = document.createElement('div');
             blockMaze.appendChild(elementMaze)
-            blockMaze.classList.add('blockMaze')
             if (map[i][j] === 'W'){
                 elementMaze.classList.add('parede')
             }
             if (map[i][j] === 'S'){
                 elementMaze.classList.add('player')
+                playerPosition = [...playerPosition, i, j];
             }
-            if (map[i][j] === ' '){
+            if (map[i][j] !== 'W' || map[i][j] !== 'S'){
                 elementMaze.classList.add('space')
             }
         }
     }
+
+    console.log(playerPosition)
 }
 
-maze()
+buildMaze()
